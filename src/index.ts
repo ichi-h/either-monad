cf29909c180eda3ee6ac5a8d9395c9690a926e61
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-02-12 19:25:16 +0900

    Add value and which functions

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ export interface Left<T> extends Which {
   readonly which: "left";
 }
 
+const value = <L, R>(either: Either<L, R>) => either["value"];
+
+const which = <L, R>(either: Either<L, R>) => either["which"];
+
 export type Either<L, R> = Left<L> | Right<R>;
 
 export const left = <L, R>(left: L): Either<L, R> => {
@@ -27,11 +31,11 @@ export const either =
   <L, R, T>(onLeft: (left: L) => T) =>
   (onRight: (right: R) => T) =>
   (either: Either<L, R>): T => {
-    switch (either.which) {
+    switch (which(either)) {
       case "left":
-        return onLeft(either.value);
+        return onLeft(value(either) as L);
       case "right":
-        return onRight(either.value);
+        return onRight(value(either) as R);
     }
   };
 
@@ -39,7 +43,7 @@ export const either =
 export const fromLeft =
   <L, R>(left: L) =>
   (either: Either<L, R>): L => {
-    if (either.which === "left") return either.value;
+    if (which(either) === "left") return value(either) as L;
     return left;
   };
 
@@ -47,16 +51,16 @@ export const fromLeft =
 export const fromRight =
   <L, R>(right: R) =>
   (either: Either<L, R>): R => {
-    if (either.which === "right") return either.value;
+    if (which(either) === "right") return value(either) as R;
     return right;
   };
 
 // isLeft:: Either a b -> Bool
 export const isLeft = <L, R>(either: Either<L, R>): boolean => {
-  return either.which === "left";
+  return which(either) === "left";
 };
 
 // isRight:: Either a b -> Bool
 export const isRight = <L, R>(either: Either<L, R>): boolean => {
-  return either.which === "right";
+  return which(either) === "right";
 };
